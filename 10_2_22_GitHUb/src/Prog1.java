3f4edd92991b872312cff0957c7aaca295fa0b0d
Pavan <Pavan@LAPTOP-QLQD14MR>
2022-02-11 17:37:45 +0530

    hiiii

--- 10_2_22_GitHUb/src/Prog1.java
+++ 10_2_22_GitHUb/src/Prog1.java
@@ -1,6 +1,17 @@
 
 public class Prog1 {
-	public static void main(String[] args) {
+	public void m1()
+	{
+		System.out.println("Testing");
+	}
+	
+	public void m2()
+	{
+		System.out.println("Batch");
+	}
+	
+	public static void main(String[] args)
+	{
 		System.out.println("hello");
 	}
 
